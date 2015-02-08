f899b9d0f6be996ac1581c11d4d260e7c0997221
Jeesmon Jacob <jeesmon@gmail.com>
2015-02-08 17:39:41 -0500

    Save font size to local storage

--- Sh'himo/content/english/script.js
+++ Sh'himo/content/english/script.js
@@ -2,5 +2,18 @@ function resizeText(multiplier) {
 	if (document.body.style.fontSize == "") {
 		document.body.style.fontSize = "1.0em";
 	}
-	document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.2) + "em";
+	var fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.2) + "em";
+    document.body.style.fontSize = fontSize;
+    window.localStorage["fontSize"] = fontSize;
+}
+
+function init() {
+    var fontSize = window.localStorage["fontSize"];
+    if(fontSize) {
+        document.body.style.fontSize = fontSize;
+    }
+}
+
+window.onload = function() {
+    init();
 }
